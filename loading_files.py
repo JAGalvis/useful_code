73c6a036b0f49dab85f7bb965bb0b47713abde3e
JAGalvis <33692396+JAGalvis@users.noreply.github.com>
2018-07-15 17:10:49 +0200

    Update loading_files.py

--- loading_files.py
+++ loading_files.py
@@ -8,7 +8,6 @@ def load_file_from_web(file_url, file_name):
 	filename = file_name
 	urlretrieve(url, filename)
 
-	
 ####
 import zipfile
 import pandas as pd
@@ -20,3 +19,21 @@ def load_csv_from_zip(zipped_file_name,csv_name):
 	archive = zipfile.ZipFile(zipped_file_name, 'r')
 	df =  pd.read_csv(archive.open(csv_name))
 	return df
+
+####
+def check_cloud_or_premise():
+    '''
+    Check if the notebook is being run on Datalab or on a local machine.
+    Returns True if Cloud, False if local
+    '''
+    import os
+    is_cloud = 'DATALAB_ENV' in list(os.environ)
+    return is_cloud
+
+####
+def how_many_cores():
+	import multiprocessing
+	cores = multiprocessing.cpu_count()
+	return cores
+
+####
